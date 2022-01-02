1831aad348ea91cd8cebad20c0befe144b5d5361
Lucas Travagin <lucas_travagin1112@hotmail.com>
2022-01-02 10:02:10 -0300

    Implementacao do dominio e Resource de Categoria

---
+++ src/main/java/lucas/travagin/vendas/domain/Category.java
@@ -0,0 +1,50 @@
+package lucas.travagin.vendas.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Category implements Serializable {
+
+
+
+    private Integer id;
+    private String description;
+
+    public Category() {
+
+    }
+
+    public Category(Integer id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/lucas/travagin/vendas/resources/CategoryResource.java
@@ -0,0 +1,27 @@
+package lucas.travagin.vendas.resources;
+
+import lucas.travagin.vendas.domain.Category;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/categories")
+public class CategoryResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Category> Listar() {
+
+        Category cat1 = new Category(1, "Informática");
+        Category cat2 = new Category(2, "Escritório");
+
+        List<Category> list = new ArrayList<>();
+        list.add(cat1);
+        list.add(cat2);
+
+        return list;
+    }
+}
